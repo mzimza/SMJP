ff7b9d88cf3fed0f0638722293fb274e894c2c2c
Maja <maja.zalewska@gmail.com>
2016-10-19 13:21:56 +0200

    minor changes

--- Swift_IDE/Hello/Sources/Hello.swift
+++ Swift_IDE/Hello/Sources/Hello.swift
@@ -3,3 +3,11 @@ struct Hello {
     var text = "Hello, World Again!"
     var howMany = 42
 }
+
+
+func sayHello() {
+    var h = Hello()
+    for i in 1...h.howMany {
+        print(h.text)
+    }
+}
--- Swift_IDE/Hello/Tests/HelloTests/HelloTests.swift
+++ Swift_IDE/Hello/Tests/HelloTests/HelloTests.swift
@@ -5,7 +5,7 @@ class HelloTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(Hello().text, "Hello, World!")
+        XCTAssertNotEqual(Hello().text, "Hello, World!")
     }
 
 
